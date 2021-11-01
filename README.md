6975b4ddc7edd9765b138656ef470ccf6c70cb4e
Fredi Machado <fredisoft@gmail.com>
2021-11-01 18:09:30 +1100

    Add very first version of the plugin

---
+++ README.md
@@ -0,0 +1,32 @@
+# Copy response or property value to the clipboard
+
+This is a simple insomnia plugin to copy the response or property value to the clipboard.
+
+## Pre-requisites
+
+This plugin requires [Insomnia](https://insomnia.rest/), the Open Source API client.
+
+## Installation
+
+1. Start Insomnia,
+2. Click "Application" -> "Preferences" and choose the "Plugins" tab,
+3. Enter `insomnia-plugin-copy-to-clipboard` and click "Install Plugin".
+
+4. Close the dialog.
+
+## Usage
+
+1. Go to a request in any collection,
+2. Add a new query item,
+3. Press Cmd/Ctrl + Space and choose "Copy To Clipboard",
+4. Click the "Copy To Clipboard" tag,
+5. In "Property to copy" set the property you want to copy, or leave it blank to copy the entire response,
+6. Click done, cross your fingers and give it a go!
+
+## Limitations
+
+* It only works with json objects when setting a property to copy
+
+## Contributing
+
+In case you want to improve it, PRs are welcome! :)
---
+++ main.js
@@ -0,0 +1,54 @@
+const electron = require("electron");
+
+let propertyToCopy = null;
+
+module.exports.templateTags = [{
+    name: "copy_to_clipboard",
+    displayName: "Copy to Clipboard",
+    description: "Copy response or property value to clipboard",
+    args: [
+        {
+            displayName: "Property to copy (empty value will copy the response)",
+            type: "string",
+            defaultValue: ""
+        }
+    ],
+    async run(context, property) {
+        propertyToCopy = property;
+    }
+}];
+
+module.exports.responseHooks = [
+    context => {
+        if (context.response.getStatusCode() !== 200) {
+            return;
+        }
+
+        try {
+            let response = "";
+            let responseBody = context.response.getBody().toString("utf-8");
+            if (!propertyToCopy || propertyToCopy.length === 0) {
+                response = responseBody;
+            } else {
+                response = JSON.parse(responseBody);
+                propertyToCopy = propertyToCopy.split(".");
+                for (var i = 0; i < propertyToCopy.length; i++) {
+                    if (response[propertyToCopy[i]] == undefined) {
+                        throw new Error(`${propertyToCopy[i]} not found`);
+                    } else {
+                        response = response[propertyToCopy[i]];
+                    }
+                }
+            }
+
+            if (typeof response === "string") {
+                electron.clipboard.write({
+                    text: response
+                });
+            }
+        } catch (error) {
+            console.log(`Error copying response or property value to clipboard with message ${error.message}`, error);
+            throw error;
+        }
+    }
+];
